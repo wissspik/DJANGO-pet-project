49869b62849cbd1b6b58713eed4d2fdf4fa72dd6
stla7 <stla7824@gmail.com>
2025-01-14 13:13:20 +0700

    /

--- firstpr/start/views.py
+++ firstpr/start/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.shortcuts import render,redirect
 from argon2 import PasswordHasher
 from .models import Person
@@ -5,6 +6,11 @@ from .models import Person
 from django.shortcuts import render, redirect
 from argon2 import PasswordHasher
 from .models import Person
+=======
+from django.shortcuts import render, redirect
+from argon2 import PasswordHasher
+from django.contrib import messagesmodels import Person
+>>>>>>> ec4bec4 (/)
 
 def d_view(request):
     ph = PasswordHasher()
@@ -16,7 +22,10 @@ def d_view(request):
         try:
             # Пытаемся найти пользователя по имени
             person = Person.objects.get(name=name)
+<<<<<<< HEAD
 
+=======
+>>>>>>> ec4bec4 (/)
             # Проверяем пароль
             if ph.verify(person.password, password):  # Сравнение пароля
                return redirect('secret')  # Если пароль совпадает, перенаправляем
@@ -27,6 +36,7 @@ def d_view(request):
         except Person.DoesNotExist:
             ### банер с предложение о регистрации
             pass
+<<<<<<< HEAD
     return render(request,'d.html')
 def registration_view(request):
 
@@ -37,12 +47,18 @@ def registration_view(request):
     # Если метод GET, отображаем форму
     return render(request, 'd.html')
 def registration_view(request):
+=======
+    return render(request,'entrance.html')
+def registration_view(request):
+    ph = PasswordHasher()
+>>>>>>> ec4bec4 (/)
     if request.method == 'POST':
         name = request.POST.get('name')
         password_1 = request.POST.get('password')
         password_2 = request.POST.get('password_2')
         exists = Person.objects.filter(name=name).exists() # истинность нахождения users в бд
         if exists:
+<<<<<<< HEAD
             return redirect('home') # пользователь есть
         #else:
          #   if password_1 == password_2: # верность введения двух паролей
@@ -54,3 +70,16 @@ def registration_view(request):
     return render(request,'registration.html')
 
 
+=======
+            messages.add_message(request,messages.WARNING, "Данный ник уже существует",extra_tags = 'error_users')
+        else:
+            if password_1 == password_2: # верность введения двух паролей
+                Person.objects.create(name = name,password = ph.hash(password_1))
+                messages.add_message(request,messages.SUCCESS,"Вы успешно зарегистировались",extra_tags = 'success')
+
+            else:
+                messages.add_message(request, messages.ERROR, "Пароли не совпадают", extra_tags='error_password')
+    return render(request,'registration.html')
+def focus_view(request):
+    return True
+>>>>>>> ec4bec4 (/)
